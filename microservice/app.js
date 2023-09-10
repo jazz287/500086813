f7e6a467e8ed74e20b379d6052ae0d9148710288
jazz287 <wadhwa.jasmine28@gmail.com>
2023-09-10 12:49:49 +0530

    add readme + clean code �

--- microservice/app.js
+++ microservice/app.js
@@ -1,33 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (request, response) => {
-    response.redirect("/numbers");
-});
-
 app.get("/numbers", async (req, res) => {
     const urls = req.query.url;
     const response = [];
     const numbers = await Promise.all(urls.map(async (url, index) => {
-        return fetch(url, { timeout : 500 }).then(async(resp) => {
-            let json = await resp.json();
-            return json['numbers'];
-        }).catch((err) => {
+        return fetch(url, { timeout : 300 }).then(async(resp)  => resp.json()).catch((err) => {
             console.log(err);
             // return empty array if any of the urls is not reachable
             return [];
         });
     }));
 
-    for (let arr in numbers){
-        response.push(...numbers[arr]);
+    for (let obj in numbers){
+        response.push(...numbers[obj].numbers);
     }
 
-    // make sure the response is sorted in ascending order
-    response.sort((a, b) => a - b);
-
-    // remove duplicate numbers
-    let filteredResponse = response.filter((item, index) => response.indexOf(item) === index);
+    // do sorting and filtering in one go
+    let filteredResponse = [...new Set(response)].sort((a, b) => a - b);
 
     res.json({ numbers: filteredResponse });
 });
