a9dd39894b4a2b670ce44d6e78466d505af00630
jazz287 <wadhwa.jasmine28@gmail.com>
2023-09-10 12:19:48 +0530

    get response from all urls

--- microservice/app.js
+++ microservice/app.js
@@ -5,8 +5,20 @@ app.get("/", (request, response) => {
     response.redirect("/numbers");
 });
 
-app.get("/numbers", (request, response) => {
-    
+app.get("/numbers", async (req, res) => {
+    const urls = req.query.url;
+    // const controllerArray = [];
+    const texts = await Promise.all(urls.map(async (url, index) => {
+        controllerArray.push(new AbortController());
+        return fetch(url, { signal: controllerArray.at(index).signal, timeout : 500 }).then(resp => resp.json())
+    }));
+
+    // // abort all controllers after 2s of timeout
+    // setTimeout(() => {
+    //     controllerArray.forEach(controller => controller.abort());
+    // }, 500);
+
+    res.json(texts);
 });
 
 app.listen(8008, () => {
